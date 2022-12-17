3629800042781c1f1e33818b4504425598aeac15
Lord-Bambucha <lordbambycha93@gmail.com>
2022-12-17 02:57:03 +0200

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const ulEl = document.querySelector("#ingredients");
+
+ingredients.map(el => { 
+
+  const liEl = document.createElement("li");
+  liEl.textContent = el;
+  ulEl.append(liEl);
+})
+
+// const liEl = document.createElement("li");
+// liEl.textContent = 'Potatoes';
+
+// ulEl.append(liEl);
+
