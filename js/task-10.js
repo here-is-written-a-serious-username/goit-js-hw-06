03736554f68758ad47d0208af7fa4569e562997f
Lord-Bambucha <lordbambycha93@gmail.com>
2022-12-28 13:02:00 +0200

    10

--- js/task-10.js
+++ js/task-10.js
@@ -6,7 +6,7 @@ const btnCreate = document.querySelector("[data-create]");
 const btnDestroy = document.querySelector("[data-destroy]");
 const divBoxes = document.querySelector("#boxes");
 
-console.log(`привіт! чи є можливість глянути інші варіанти рішення цієї задачки? сорі за спам)`)
+
 
 divBoxes.style = 'display: flex; gap: 5px; flex-wrap: wrap;';
 
@@ -23,53 +23,37 @@ function onBtnCreateClick() {
 
 function createBoxes(amount) {
   const divBoxesArray = [];
+  let size = 0;
 
   if (divBoxes.lastElementChild === null) {
-    let size = 20;
-
-    for (let i = 1; i <= amount; i += 1) {
-      const divBox = document.createElement("div");
-      divBox.style.backgroundColor = getRandomHexColor();
-
-      size += 10;
-      const sizeBox = `${size}px`;
-
-      divBox.style.width = sizeBox;
-      divBox.style.height = sizeBox;
-
-      console.log(size);
-
-      divBoxesArray.push(divBox);
-    }
-  }
-  else {
-    let size = Number.parseInt(divBoxes.lastElementChild.style.width);
+    size = 20;
+  } else {
+    size = Number.parseInt(divBoxes.lastElementChild.style.width);
     // const elHeight = divBoxes.lastElementChild.style.height;
-    for (let i = 1; i <= amount; i += 1) {
-      const divBox = document.createElement("div");
-      divBox.style.backgroundColor = getRandomHexColor();
-
-      size += 10;
-      const sizeBox = `${size}px`;
-
-      divBox.style.width = sizeBox;
-      divBox.style.height = sizeBox;
-
-      console.log(size);
-
-      divBoxesArray.push(divBox);
-    }
   }
+
+  for (let i = 1; i <= amount; i += 1) {
+    const divBox = document.createElement("div");
+    divBox.style.backgroundColor = getRandomHexColor();
+
+    size += 10;
+    const sizeBox = `${size}px`;
+
+    divBox.style.width = sizeBox;
+    divBox.style.height = sizeBox;
+
+    console.log(size);
+
+    divBoxesArray.push(divBox);
+  }
+
   divBoxes.append(...divBoxesArray);
 }
-
 
 
 function onBtnDestroyClick() {
   divBoxes.innerHTML = '';
 }
-
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
